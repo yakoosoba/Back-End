0897dd33a9a9d76555ae662a8be9ee6b4df492cc
Beatriz Almeida <yakoosoba@gmail.com>
2026-05-28 19:59:32 -0500

    contador

---
+++ App/src/Repete.java
@@ -0,0 +1,11 @@
+public class Repete {
+    
+    public static void main(String[] args) throws Exception {
+
+        int contador = 0;
+        while (contador < 3) {
+            System.out.println("Olá");
+            contador++;
+        }
+    }
+}
